7c1b9d42643f631e28a1c3ccf7519cf9b86ad5bc
bakhromjon993 <baxrom.yuldoshev@gmail.com>
2021-04-28 17:53:04 +0500

    Assignment week3

---
+++ week3/w3.py
@@ -0,0 +1,53 @@
+# total = 71
+# dropouts = 12
+# dismissed = 15
+#
+# if total < dropouts:
+#     print("Too many dropouts! The course is doomed!")
+#
+# if total > dropouts:
+#     print("Not enough dropouts! Need to dismiss some!")
+#
+# if total < dismissed:
+#     print("The course is finished! No students remain!")
+#
+# if total > dismissed:
+#     print("The course rolls on!")
+#
+# dismissed += 56
+#
+# if total >= dismissed:
+#     print("Total is greater than or equal to dismissed.")
+#
+# if total <= dismissed:
+#     print("Total is less than or equal to dismissed.")
+#
+# if total == dismissed:
+#     print("Everyone is dismissed from the course.")
+
+# def add(a, b):
+#     print(f"ADDING {a} + {b}")
+#     return a + b
+#
+# add(5, 2)
+#
+#
+# def divide(a, b):
+#     c = a/b
+#     print(c)
+#     return c
+#
+# square(10,2)
+
+# def farToCel(f):
+#     c = (f - 32) * 5.0/9.0
+#     print("Temperature:", f, "Fahrenheit = ", c, "C")
+#     return c
+#
+# Fahrenheit = int(input("Enter a temperature in Fahrenheit: "))
+# farToCel(Fahrenheit)
+
+def say_hello(name):
+    print(f"hello {name}")
+
+say_hello('Bahrom')
